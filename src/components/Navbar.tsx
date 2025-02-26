1498ea3ab9bd64613e25cbf05f65122defc7e2c2
nana0sei <frimpongnanao@gmail.com>
2025-02-26 22:01:27 +0000

    color mode cleanup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,41 @@
+import { useEffect, useState } from "react";
+import { BsFillMoonFill, BsSun } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import logoB from "../assets/logoB.png";
-import ThemeSwitch from "./ThemeSwitch";
+import logoW from "../assets/logoW.png";
 
 const Navbar = () => {
+  const localTheme = localStorage.getItem("portfolio-theme") || "light";
+  const [theme, setTheme] = useState(localTheme);
+
+  useEffect(() => {
+    localStorage.setItem("portfolio-theme", theme);
+    document.querySelector("html")?.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <div className="flex justify-between items-center sticky top-0 p-3 mb-5 bg-base-100">
       <Link to="/">
-        <img src={logoB} alt="nana icon" className="w-12" />
+        <img
+          src={theme === "light" ? logoB : logoW}
+          alt="nana icon"
+          className="w-12"
+        />
       </Link>
-
-      <ThemeSwitch />
+      {/* theme switch */}
+      <div className="flex items-center gap-1">
+        <input
+          type="checkbox"
+          className="toggle toggle-info"
+          checked={theme === "light"}
+          onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+        />
+        {theme === "light" ? (
+          <BsSun size="20px" />
+        ) : (
+          <BsFillMoonFill size="15px" />
+        )}
+      </div>{" "}
     </div>
   );
 };
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BsFillMoonFill, BsSun } from "react-icons/bs";
 
 const ThemeSwitch = () => {
   const localTheme = localStorage.getItem("portfolio-theme") || "light";
@@ -11,12 +12,19 @@ const ThemeSwitch = () => {
 
   return (
     <>
-      <input
-        type="checkbox"
-        className="toggle toggle-info"
-        checked={theme === "light"}
-        onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
-      />
+      <div className="flex items-center gap-1">
+        <input
+          type="checkbox"
+          className="toggle toggle-info"
+          checked={theme === "light"}
+          onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+        />
+        {theme === "light" ? (
+          <BsSun size="20px" />
+        ) : (
+          <BsFillMoonFill size="15px" />
+        )}
+      </div>
     </>
   );
 };
