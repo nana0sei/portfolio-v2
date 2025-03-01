0c7409845e193f179ca96074ad6b6a7e84ba3e83
nana0sei <frimpongnanao@gmail.com>
2025-03-01 19:20:01 +0000

    updating images

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,32 +1,15 @@
-import casestody from "../assets/art/casestody.webp";
-import city from "../assets/art/city.webp";
-import gt from "../assets/art/gt-01.webp";
-import krpt from "../assets/art/korrupt.webp";
-import onfire from "../assets/art/onfire.webp";
-import rita from "../assets/art/ritalucia.webp";
-import sony from "../assets/art/sony23.webp";
-import sony24 from "../assets/art/sony24.webp";
+import stolen from "../assets/art/stolen.png";
+import leomie from "../assets/art/leomie.png";
 
 const photos = [
   {
-    image: gt,
-    description:
-      "'ghost town' - solis4evr (2024). additional character designs by wooshyworld",
+    image: leomie,
+    description: "leomie anderson",
   },
   {
-    image: sony24,
-    description:
-      "tribe of god x sony music publishing (2024). designed with jude dontoh and gregory abloh",
+    image: stolen,
+    description: "stolen arts jacket",
   },
-  { image: onfire, description: "'on fire/cool me down' - saezn (2023)" },
-  { image: rita, description: "twitch emotes - lady legasus (2023)" },
-  {
-    image: sony,
-    description: "sony music publishing (2023). designed with jude dontoh",
-  },
-  { image: krpt, description: "krpt doberman tee (2023)" },
-  { image: city, description: "city of angels - saezn (2022)" },
-  { image: casestody, description: "casestody gameboy case (2022)" },
 ];
 
 export default photos;
