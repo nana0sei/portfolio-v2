a21abf6b7d74ade8050987c7c8d4a6ac86573d57
nana0sei <frimpongnanao@gmail.com>
2025-04-18 16:46:28 +0000

    cleanups

--- src/hooks/useCloudinary.ts
+++ src/hooks/useCloudinary.ts
@@ -1,7 +1,5 @@
 import { Cloudinary } from "@cloudinary/url-gen";
 import { fill } from "@cloudinary/url-gen/actions/resize";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 
 const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 const cld = new Cloudinary({
@@ -18,14 +16,3 @@ const useCloudinary = (imageUrl: string) => {
 };
 
 export default useCloudinary;
-
-const getImageList = async () => {
-  const url = `https://res.cloudinary.com/${cloudName}/image/list/tie.json`;
-  return await axios.get(url).then((res) => res.data);
-};
-
-export const useImages = () =>
-  useQuery({
-    queryKey: ["images"],
-    queryFn: () => getImageList(),
-  });
