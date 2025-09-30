67cf9e8874e4f2e90d19e9ee7923e2f5403c5383
nana0sei <frimpongnanao@gmail.com>
2025-09-30 09:40:43 +0000

    descriptions

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,7 +6,7 @@ import { Project } from "../entities/Project";
 const projects: Project[] = [
   {
     name: "faith hq",
-    description: "a church management web app",
+    description: "a church management solution",
     image: fhq,
     link: "https://www.logicielghana.com/platorms/platforms/fathhq",
     git: "",
