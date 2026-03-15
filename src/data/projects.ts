97b95e8da14b253c2dc9ce2d9553d6e34500bdcc
nana0sei <frimpongnanao@gmail.com>
2026-03-15 12:35:08 +0000

    cleanup

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,5 @@
-import portfolio from "../assets/portfolio.png";
-import fhq from "../assets/fhq.png";
 import loangrid from "../assets/loangrid.png";
-import lseza from "../assets/lseza.png";
+import portfolio from "../assets/portfolio.png";
 
 import { Project } from "../entities/Project";
 
@@ -17,40 +15,12 @@ const projects: Project[] = [
   },
 
   {
-    name: "lseza license application portal",
-    description:
-      "a web app that allows users to apply for and manage special economic zone licenses in liberia.",
-    image: lseza,
-    link: "https://liberiaseza.gov.lr/",
-    git: "",
-    tools: ["nextjs", "tailwind", "hero ui"],
-  },
-
-  {
-    name: "faith hq",
-    description:
-      "a comprehensive church management solution built for modern religious institutions. designed to simplify administration and enhance community connection, faith hq empowers churches to operate efficiently, grow sustainably, and deepen their impact.",
-    image: fhq,
-    link: "https://www.logicielghana.com/platorms/platforms/fathhq",
-    git: "",
-    tools: ["nextjs", "tailwind"],
-  },
-  {
     name: "portfolio",
     description: "here.",
     image: portfolio,
     link: "https://www.nanaosei.xyz",
     git: "",
     tools: ["react", "typescript", "tailwind"],
-  },
-
-  {
-    name: "vidly",
-    description: "rest api and tests for a fictional movie rental service.",
-    image: "",
-    link: "",
-    git: "https://github.com/nana0sei/vidly",
-    tools: ["node.js", "express", "mongodb", "jest"],
   },
 ];
 
